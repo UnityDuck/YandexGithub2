98814dc0f9c294a9cdedefd167e56e256fe94118
UnityDuck <135706672+UnityDuck@users.noreply.github.com>
2025-02-28 19:03:37 +0300

    commit

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
+import random
 import sys
-from main_window import MainWindow
-from PyQt6.QtWidgets import QApplication
+
+from PyQt6.QtCore import QRect
+from PyQt6.QtGui import QColor, QPainter
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+from design import Design
+
+
+class MainWindow(QMainWindow, Design):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.pushButton.clicked.connect(self.create_circles)
+
+        self.circles = []
+
+    def create_circles(self):
+        x = random.randint(20, 180)
+        y = random.randint(20, 250)
+        radius = random.randint(20, 50)
+
+        if x - radius < 0: x = radius
+        if y - radius < 0: y = radius
+        if x + radius > 200: x = 200 - radius
+        if y + radius > 300: y = 300 - radius
+
+        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+        self.circles.append((x, y, radius, color))
+
+        self.update()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+
+        for (x, y, radius, color) in self.circles:
+            painter.setBrush(color)
+            painter.drawEllipse(QRect(x - radius, y - radius, radius * 2, radius * 2))
+
+        painter.end()
 
 
 if __name__ == '__main__':
@@ -8,3 +49,4 @@ if __name__ == '__main__':
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
+
